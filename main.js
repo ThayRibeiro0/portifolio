c63fea8321f4b8a89bfd335eed434d388ed5f03c
ThayRibeiro0 <thaysmoiaribeiro@gmail.com>
2022-04-18 23:03:29 -0300

    fit: go

---
+++ main.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import { engine } from 'express-handlebars';
+
+const app = express();
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+app.listen(8080);
