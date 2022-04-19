eb021cc65dde08ae7720b4fa402c185a7c709bb8
ThayRibeiro0 <thaysmoiaribeiro@gmail.com>
2022-04-18 23:33:10 -0300

    feat: creating a new document

--- main.js
+++ main.js
@@ -7,4 +7,20 @@ app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
+app. get('/', (req, res) => {
+    res.redirect('/about');
+})
+
+app.get('/about', (req, res) => {
+    res.render('about');
+})
+
+app.get('/resume', (req, res) => {
+    res.render('resume');
+})
+
+app.get('/project', (req, res) => {
+    res.render('project');
+})
+
 app.listen(8080);
